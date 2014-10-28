849539dd6081a48d5be8eb9c81bafb0095381fa6
Yun Zhou <zhyun1990@gmail.com>
2014-10-27 18:17:12 -0700

    version 1, add all essential sections think about what projects to include as web developer

--- js/helper.js
+++ js/helper.js
@@ -88,7 +88,11 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
-});
+  var x = loc.PageX;
+  var y = loc.PageY;
+
+  logClicks(x, y);
+})
 
 
 
@@ -173,6 +177,7 @@ function initializeMap() {
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
       // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -235,11 +240,11 @@ Uncomment all the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window 
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
